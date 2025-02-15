7826a22e1e89f6817ab03ae22bf153292d7a6cbb
Dhanushmathan <lovelymathan2394@gmail.com>
2025-02-15 21:24:26 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,8 +27,8 @@ const Login = () => {
         <div className="bg-[#ede7f6] min-h-screen flex flex-col items-center px-6 overflow-y-hidden">
             <nav className="w-full max-w-6xl flex justify-between items-center py-6">
                 <div className='text-[#6c66ff] flex items-center'>
-                    <img src={chatLogo} alt="logo" className='xs:w-7 w-14 mr-3' />
-                    <h1 className="xs:text-xl text-3xl font-bold">ChatHub</h1>
+                    <img src={chatLogo} alt="logo" className='xs:w-7 sm:w-14 mr-3' />
+                    <h1 className="xs:text-xl sm:text-3xl font-bold">ChatHub</h1>
                 </div>
                 <div className="hidden md:flex space-x-6">
                     <a href="#" className="text-gray-700">Home</a>
@@ -37,13 +37,13 @@ const Login = () => {
                     <a href="#" className="text-gray-700">Contact</a>
                 </div>
                 <div className="space-x-4">
-                    <button className="px-4 py-2 border border-light-blue hover:bg-light-blue hover:text-white text-[#6c44ff] rounded-md cursor-pointer font-semibold xs:px-2 xs:py-1 xs:text-sm">Login</button>
-                    <button className="px-4 py-2 bg-light-blue hover:bg-[#6c44ff] cursor-pointer text-white rounded-md font-semibold xs:px-2 xs:py-1 xs:text-sm">Sign in</button>
+                    <button className="sm:px-4 sm:py-2 border border-light-blue hover:bg-light-blue hover:text-white text-[#6c44ff] rounded-md cursor-pointer font-semibold xs:px-2 xs:py-1 xs:text-sm">Login</button>
+                    <button className="sm:px-4 sm:py-2 bg-light-blue hover:bg-[#6c44ff] cursor-pointer text-white rounded-md font-semibold xs:px-2 xs:py-1 xs:text-sm">Sign in</button>
                 </div>
             </nav>
 
-            <div className="flex flex-col md:flex-row items-center xs:mt-8 mt-16 max-w-6xl">
-                <div className="md:w-1/2 flex items-center xs:mr-0 mr-20">
+            <div className="flex flex-col md:flex-row items-center xs:mt-8 sm:mt-16 max-w-6xl">
+                <div className="md:w-1/2 flex items-center xs:mr-0 sm:mr-20">
                     <form action="" className="w-full max-w-md space-y-4" onSubmit={handleSubmit}>
                         <div>
                             <h2 className="text-2xl font-bold text-gray-800 text-center">Welcome Back!</h2>
